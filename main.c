60e86f1389d867ddae6690aa10622ec45e0ad2b9
Anbiya Popal <anbiya2004@gmail.com>
2022-02-24 13:56:53 +0100

    add quicksort

--- main.c
+++ main.c
@@ -38,23 +38,86 @@ void bubble_sort(int *input_array, size_t size)
     }
 }
 
+void insertion_sort(int *input_array, size_t size)
+{
+    int i = 1;
+    while (i < size)
+    {
+        int j = i - 1;
+        while (j >= 0 && input_array[j] > input_array[j + 1])
+        {
+            swap(input_array, j, j + 1);
+            j--;
+        }
+        
+        i++;
+    }
+    return;
+}
+
+void quick_sort(int *input_array, int idx_start, int idx_end)
+{
+    if (idx_end - idx_start <= 1) { return; }
+
+    int pivot_idx = idx_start;
+    
+    for (int i = idx_start + 1; i < idx_end; i++) 
+    {
+        if (input_array[i] < input_array[pivot_idx])
+        {
+            swap(input_array, pivot_idx, pivot_idx + 1);
+
+            if (pivot_idx + 1 != i)
+            {
+                swap(input_array, pivot_idx, i);
+            }
+
+            pivot_idx++; 
+        }
+    }
+
+    quick_sort(input_array, idx_start, pivot_idx);
+    quick_sort(input_array, pivot_idx + 1, idx_end);
+}
+
+void timed_sort(char *sorting_mode, int num_of_values, int max_value, int print_array)
+{
+    int array[num_of_values];
+    randomize_array(array, num_of_values, max_value); 
+
+    time_t start = time(NULL); // record time before sorting
+
+    if (sorting_mode == "bubble")
+    {
+        bubble_sort(array, num_of_values);
+    }
+    else if (sorting_mode == "insertion")
+    {
+        insertion_sort(array, num_of_values);
+    }
+    else if (sorting_mode == "quick")
+    {
+        quick_sort(array, 0, num_of_values);
+    } 
+
+    time_t end = time(NULL); // record time again
+
+    if (print_array == 1)
+    {
+        for (int i = 0; i < num_of_values; i++)
+        {
+            printf("%ld\n", array[i]);
+        }
+    }
+
+    printf("Executed in %ld using %s sort\n", end - start, sorting_mode); 
+}
+
 int main(void) 
 {
-    int array[100000];
-    randomize_array(array, sizeof array / sizeof *array, 1000);
-
-    time_t start = time(NULL);
+    //timed_sort("bubble",    100000, 10000, 0);
+    //timed_sort("insertion", 100000, 10000, 0);
+    timed_sort("quick",       900000, 10000, 0);
  
-    bubble_sort(array, sizeof array / sizeof *array);
-
-    time_t end = time(NULL);
-
-    printf("Executed in %ld seconds\n", end - start);
-    /*
-    for (int i = 0; i < sizeof array / sizeof *array; i++) {
-        printf("%u\n", array[i]);
-    } 
-    */
-
     return 0;
 }
